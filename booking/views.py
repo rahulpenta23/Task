078c07a513b94fcc9e7908020a84035f2542f484
penta abhishek <pabhishek4848@gmail.com>
2026-03-06 09:56:11 +0530

    changes done

--- booking/views.py
+++ booking/views.py
@@ -2,30 +2,32 @@ from django.shortcuts import render, redirect
 from .models import Slot
 
 
+# View function to display all available slots
 def view_slots(request):
 
     slots = Slot.objects.all()
-
     service = request.GET.get("service")
-
     if service:
         slots = slots.filter(name=service)
 
     return render(request, "slots.html", {"slots": slots})
 
 
+# View function to book a slot
 def book_slot(request, slot_id):
 
     slot = Slot.objects.get(id=slot_id)
 
     slot.is_booked = True
+
     slot.save()
 
     return redirect("view_slots")
 
 
+# View function to display booked slots
 def booked_slots(request):
 
+    
     slots = Slot.objects.filter(is_booked=True)
-
     return render(request, "booked_slots.html", {"slots": slots})
