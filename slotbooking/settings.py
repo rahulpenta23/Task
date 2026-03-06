3e20c1205ef26e60ec6fc2e6c5eb33e0078f0596
penta abhishek <pabhishek4848@gmail.com>
2026-03-06 10:48:40 +0530

    hosts allow

--- slotbooking/settings.py
+++ slotbooking/settings.py
@@ -80,14 +80,13 @@ load_dotenv()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+        'NAME': os.getenv("MYSQLDATABASE"),
+        'USER': os.getenv("MYSQLUSER"),
+        'PASSWORD': os.getenv("MYSQLPASSWORD"),
+        'HOST': os.getenv("MYSQLHOST"),
+        'PORT': os.getenv("MYSQLPORT"),
     }
 }
-
 
 
 # Password validation
